b75f6b4f7ff0c7b734587bcb294e531681e3d00d
AsHim Shrestha <ashimsth89@gmail.com>
2025-05-03 16:27:13 +0545

    Add author name to the list of features in the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
+            ASHIM SHRESTHA
           </li>
         </ol>
 
